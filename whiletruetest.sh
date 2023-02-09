10e013c29b0dc17a3684e798163985b1f823cec6
kraken <70107358+Kraken908@users.noreply.github.com>
2023-02-09 14:06:46 -0700

    Create whiletruetest.sh

---
+++ whiletruetest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash                                                                     
+while true                                                                      
+do                                                                              
+  read -p "Do you want to quit (y/n)? " answer                                  
+  if [ "$answer" == "y" ]                                                       
+  then                                                                          
+    break                                                                       
+  fi                                                                            
+done                                                                            
+      
